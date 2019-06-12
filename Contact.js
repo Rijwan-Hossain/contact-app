3074b07e58ad2a4098a4da3a8d3543b158d49084
Rijwan Hossain <rijyan.cse.152@gmail.com>
2019-06-12 11:01:53 -0700

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,14 @@
+
+class Contact { 
+    constructor({name, phone, email = null}) { 
+        this.name = name; 
+        this.phone = phone; 
+        this.email = email; 
+    } 
+} 
+
+export default Contact 
+
+
+
+
